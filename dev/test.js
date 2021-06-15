8040988563179faf330d4b3964e06815072610ee
Ray Chidaz <raychidaz@gmail.com>
2021-06-15 23:53:07 +0100

    Finish building out initial blockchain data structure Created initial blockchain data structure with following methods:- createNewBlock, getLastBlock, createNewTransaction, hashBlock and proofOfWork

---
+++ dev/test.js
@@ -0,0 +1,26 @@
+import Blockchain from './blockchain.js';
+
+const bitcoin = new Blockchain();
+console.log(bitcoin);
+
+// const previousBlockHash = '0909IJBCHSHUEUE38477YR66';
+
+// const currentBlockData = [
+//   {
+//     amount: 10,
+//     sender: 'ASH009JSHHDHDHYYU',
+//     recipient: 'ANDYHH9CWU88883311',
+//   },
+//   {
+//     amount: 30,
+//     sender: 'RAY009JSHHDHDHYYU',
+//     recipient: 'SPARKSHH9CWU88883311',
+//   },
+//   {
+//     amount: 300,
+//     sender: 'QUEEN009JSHHDHDHYYU',
+//     recipient: 'TONYHH9CWU88883310',
+//   },
+// ];
+
+// console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
